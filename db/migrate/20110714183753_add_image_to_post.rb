2703511dc4ce6c94004d08b25d6df67e189b7f51
Sarah Rohm <sarahjrohm@gmail.com>
2011-07-22 16:01:35 +0200

    Picture page working

--- db/migrate/20110714183753_add_image_to_post.rb
+++ db/migrate/20110714183753_add_image_to_post.rb
@@ -1,7 +1,7 @@
 class AddImageToPost < ActiveRecord::Migration
-#  def self.up
- #   add_column :posts, :image, :string
-  #end
+  def self.up
+    add_column :posts, :image, :string
+  end
 
   def self.down
     remove_column :posts, :image
--- db/migrate/20110720134429_create_albums.rb
+++ db/migrate/20110720134429_create_albums.rb
@@ -1,20 +1,20 @@
 class CreateAlbums < ActiveRecord::Migration
   def self.up
- #   create_table :albums do |t|
-  #    t.string   "title",                                                      :null => false
-   #   t.string   "link",                                                   :null => false
-   #   t.text     "link",                                                       :null => false
-    #  t.text     "link_html",                                                  :null => false
-     # t.boolean  "active",                  :default => true,                  :null => false
-   #   t.integer  "approved_comments_count", :default => 0,                     :null => false
-    #  t.string   "cached_tag_list"
-     # t.datetime "published_at"
-   #   t.datetime "created_at"
-    #  t.datetime "updated_at"
-     # t.datetime "edited_at",                                                  :null => false
-    #end
+    create_table :albums do |t|
+      t.string   "title",                                                      :null => false
+      t.string   "link",                                                   :null => false
+      t.text     "link",                                                       :null => false
+      t.text     "link_html",                                                  :null => false
+      t.boolean  "active",                  :default => true,                  :null => false
+      t.integer  "approved_comments_count", :default => 0,                     :null => false
+      t.string   "cached_tag_list"
+      t.datetime "published_at"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.datetime "edited_at",                                                  :null => false
+    end
     
-    #  add_index "albums", ["published_at"], :name => "index_albums_on_published_at"
+      add_index "albums", ["published_at"], :name => "index_albums_on_published_at"
   end
   
 
